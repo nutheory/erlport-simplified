38c8023e2fdf59a3c51881fad14768df1cb95dd0
Derek Rush <drush81@gmail.com>
2018-08-08 00:51:24 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='erlport',
+      version='0.1',
+      description='erlport revision',
+      url='http://github.com/nutheory/erlport-simplified',
+      author='Nutheory',
+      author_email='drush@nutheory.com',
+      license='MIT',
+      packages=['erlport'],
+      zip_safe=False)
